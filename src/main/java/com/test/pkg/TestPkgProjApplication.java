a68ed6b0305ff65b3016181df03af1ab5e90016f
testriaz <riaz.engg@gmail.com>
2023-05-11 12:43:51 +0530

    test

---
+++ src/main/java/com/test/pkg/TestPkgProjApplication.java
@@ -0,0 +1,13 @@
+package com.test.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPkgProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPkgProjApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/test/pkg/implement/Test.java
@@ -0,0 +1,13 @@
+package com.test.pkg.implement;
+
+public class Test {
+
+        public static void display(String param){
+            System.out.println("You parameter is " + param);
+        }
+
+        public void someFunc(){
+            System.out.println("Testing.someFunc");
+        }
+
+}
